abdfc73025dc7f0c84da81083231172fba327b4a
nielsengg <gustavogn427@gmail.com>
2025-07-18 21:34:25 -0300

    Add more documentation

--- main.c
+++ main.c
@@ -1,9 +1,18 @@
-#include <stdio.h>
-#include <string.h>
-#include <ctype.h>
-#include <stdbool.h>
+/*
+ === Marginalia ===
 
-#include <curl/curl.h>
+ * Requires: C compiler (like GCC/Clang), cURL and cJSON libraries, cacert.pem file, internet connection.
+ * Ensures: Cataloging and management of read books (notes, ratings, reviews), persistent local data storage, and external book searches via API through a simple terminal interface.
+ 
+ * Nielsen, G.G.
+ * 2025 - CAP
+ */
+
+#include <stdio.h> // Standard input and output
+#include <string.h> // String manipulation functions
+#include <ctype.h> // Character classification and transformation
+#include <stdbool.h> // Boolean type support
+#include <curl/curl.h> // For making HTTP requests and interacting with URLs
 
 #include "marginalia.h"
 
